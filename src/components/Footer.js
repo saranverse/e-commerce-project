7368e6db7849ab8cb1f6d6b00fdd325617c6399e
saranverse <sarankasi4@gmail.com>
2024-12-30 09:56:59 +0530

    saran

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return <footer className="py-1 bg-dark">
+    <p class="text-center text-white mt-1 ">
+  RANZcart - 2024-2025, All Rights Reserved
+    </p>
+  </footer>
+}
---
+++ src/pages/Cart.js
@@ -0,0 +1,141 @@
+import { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import {toast} from 'react-toastify'
+
+export default function Cart({ cartItems, setCartItems }) {
+
+    const[complete,setComplete]= useState(false);
+
+    function increaseQty(item) {
+        if (item.product.stock === item.qty) return;
+        const updatedItems = cartItems.map((i) => {
+            if (i.product._id === item.product._id) {
+                i.qty++;
+            }
+            return i;
+        });
+        setCartItems(updatedItems);
+    }
+
+    function decreaseQty(item) {
+        if (item.qty > 1) {
+            const updatedItems = cartItems.map((i) => {
+                if (i.product._id === item.product._id) {
+                    i.qty--;
+                }
+                return i;
+            });
+            setCartItems(updatedItems);
+        }
+    }
+
+    function removeItem(item) {
+        const updatedItems = cartItems.filter(i => i.product._id !== item.product._id);
+        setCartItems(updatedItems);
+    }
+
+    function placeOrderHandler (){
+        fetch(process.env.REACT_APP_API_URL+'/order',{
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            body: JSON.stringify(cartItems)
+        })
+        .then(()=>{
+            setCartItems([]);
+            setComplete(true);
+            toast.success('Order Success!')
+       })
+    }
+
+    const subtotal = cartItems.reduce((acc, item) => acc + item.qty, 0);
+    const totalPrice = cartItems.reduce((acc, item) => acc + item.qty * item.product.price, 0);
+
+    return cartItems.length > 0 ? <Fragment>
+         <div className="container container-fluid">
+    <h2 className="mt-5">
+        Your Cart: <b>{cartItems.length} items</b>
+    </h2>
+
+    <div className="row d-flex justify-content-between">
+        <div className="col-12 col-lg-8">
+            {cartItems.map((item) => (
+                <Fragment key={item.product._id}>
+                    <hr />
+                    <div className="cart-item">
+                        <div className="row">
+                            <div className="col-4 col-lg-3">
+                                <img
+                                    src={item.product.images[0].image}
+                                    alt={item.product.name}
+                                    height="90"
+                                    width="115"
+                                />
+                            </div>
+
+                            <div className="col-5 col-lg-3">
+                                <Link to={"/product/" + item.product._id}>
+                                    {item.product.name}
+                                </Link>
+                            </div>
+
+                            <div className="col-4 col-lg-2 mt-4 mt-lg-0">
+                                <p id="card_item_price">${item.product.price}</p>
+                            </div>
+
+                            <div className="col-4 col-lg-3 mt-4 mt-lg-0">
+                                <div className="stockCounter d-inline">
+                                    <span
+                                        className="btn btn-danger minus"
+                                        onClick={() => decreaseQty(item)}
+                                    >
+                                        -
+                                    </span>
+                                    <input
+                                        type="number"
+                                        className="form-control count d-inline"
+                                        value={item.qty}
+                                        readOnly
+                                    />
+                                    <span
+                                        className="btn btn-primary plus"
+                                        onClick={() => increaseQty(item)}
+                                    >
+                                        +
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div className="col-4 col-lg-1 mt-4 mt-lg-0">
+                                <i
+                                    id="delete_cart_item"
+                                    className="fa fa-trash btn btn-danger"
+                                    onClick={() => removeItem(item)}
+                                ></i>
+                            </div>
+                        </div>
+                    </div>
+                </Fragment>
+            ))}
+        </div>
+
+        <div className="col-12 col-lg-3 my-4">
+            <div id="order_summary">
+                <h4>Order Summary</h4>
+                <hr />
+                <p>
+                    Subtotal: <span className="order-summary-values">{subtotal} (Units)</span>
+                </p>
+                <p>
+                    Est. total:{" "}
+                    <span className="order-summary-values">${totalPrice.toFixed(2)}</span>
+                </p>
+                <hr />
+                <button id="checkout_btn" onClick={placeOrderHandler} className="btn btn-primary btn-block">
+                    Place Order
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+</Fragment> : (!complete ? <h2 className="mt-5">Your cart is empty!</h2>:<Fragment> <h2 className="mt-5">Order complete!</h2><p>Your order has been placed succesfully.</p></Fragment>)
+}
